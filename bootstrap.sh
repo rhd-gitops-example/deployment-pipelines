0f23ed23a19a51a39854e926fa64a664d9a3361a
Kevin McDermott <bigkevmcd@gmail.com>
2020-01-16 14:06:08 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -49,7 +49,7 @@ sed $SED_OPTIONS "s|GITHUB_STAGE_REPO|${GITHUB_STAGE_REPO}|g" 08-eventlisteners/
 sed $SED_OPTIONS "s|APP_NAME|${APP_NAME}|g" argocd/argo-app.yaml
 sed $SED_OPTIONS "s|DEPLOYMENT_PATH|${DEPLOYMENT_PATH}|g" 07-cd/*.yaml
 
-oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.9.2/release.yaml
+oc apply -f https://github.com/tektoncd/pipeline/releases/download/v0.9.1/release.yaml
 oc apply -f https://github.com/tektoncd/triggers/releases/download/v0.1.0/release.yaml
 oc new-project dev-environment
 oc new-project stage-environment
